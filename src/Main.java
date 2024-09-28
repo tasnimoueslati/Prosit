5dc3d64d4dda308b2f2007c7d89db106840c5e97
tasnimoueslati <tasnim.oueslati@istic.ucar.tn>
2024-09-29 00:29:17 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int nbrCages = 20 ;
+        String zooName ="my zoo";
+
+        // write your code here
+        public static void afficher(){
+            System.out.println(zooName + " comport " + nbrCages);
+        }
+
+
+    }
+}
